5dc29960d86ea0b6e4d1016ffd76397d75d3e0ec
Alexei Zheltok <alexeizhautok@gmail.com>
2020-12-22 00:15:43 +0300

    modified pom

--- Framework/src/test/java/service/TestDataReader.java
+++ Framework/src/test/java/service/TestDataReader.java
@@ -3,9 +3,13 @@ package service;
 import java.util.ResourceBundle;
 
 public class TestDataReader {
-//    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
-    private static ResourceBundle resourceBundle = ResourceBundle.getBundle("dev");
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
 
-    public static String getTestData(String key){ return resourceBundle.getString(key); }
+
+    public static String getTestData(String key){
+        if(resourceBundle == null)
+            return ResourceBundle.getBundle("dev").getString(key);
+        return resourceBundle.getString(key);
+    }
 
 }
